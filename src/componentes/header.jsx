47c5985f7ba7f55c2bd29da2dd8adb37fbcb6152
simon <simonscheffer00@gmail.com>
2025-03-01 18:42:34 -0300

    Aplicacion de estilos en el header

--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom"
+import "./header.css"
 
 const Header = ()=>{
     return(
         <header>
+
             <img 
             src="https://preview.redd.it/tener-una-imagen-random-hace-que-la-publicaci%C3%B3n-tenga-m%C3%A1s-v0-6frn21rqrtoc1.jpeg?auto=webp&s=5f31ac57b8ba0cf15746c8d660b58ee0b68a43d1"
              alt="imagen random" />
+           
              <nav>
                 <ul>
                     <li><Link to="/">Inicio</Link></li>
